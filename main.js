ecc64bae0035765c42df3231f1e25ce2b67dba96
Nicolas Souza <nicolas_s_de-souza@estudante.sesisenai.org.br>
2024-02-25 21:48:49 -0300

    Firts commit

---
+++ main.js
@@ -0,0 +1,12 @@
+function exibirCaixaTexto1() {
+    document.getElementById('projetos-lista1').innerHTML = "AINDA EM CONSTRUÇÃO" 
+}
+
+function exibirCaixaTexto2() {
+    document.getElementById('projetos-lista2').innerHTML = "AINDA EM CONSTRUÇÃO" 
+}
+
+function exibirCaixaTexto3() {
+    document.getElementById('projetos-lista3').innerHTML = "AINDA EM CONSTRUÇÃO" 
+}
+
